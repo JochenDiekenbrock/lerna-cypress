7668e93ab0fadb4bd66fcb00c26085ec83f5df01
Jochen Diekenbrock <jochen.diekenbrock@tui.com>
2021-04-23 17:01:58 +0200

    add cypress

--- packages/react-app/src/App.test.tsx
+++ packages/react-app/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import * as React from 'react'
+import {mount} from '@cypress/react'
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it('App', () => {
+    mount(<App/>)
+    cy.get('button').contains('Test button').click()
+})
+
